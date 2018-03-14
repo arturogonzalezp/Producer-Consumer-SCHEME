f5723180de0d1db7d1c15802b0eeea5af9cbfff1
César Arturo González <a01261064@itesm.mx>
2018-03-13 22:21:01 -0600

    ErrorDialog implemented

--- SchemeProducerConsumer/src/schemeproducerconsumer/MainWindowController.java
+++ SchemeProducerConsumer/src/schemeproducerconsumer/MainWindowController.java
@@ -1,28 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package schemeproducerconsumer;
 
-import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXChipView;
-import com.jfoenix.controls.JFXDialog;
-import com.jfoenix.controls.JFXDialogLayout;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
-import javafx.scene.text.Text;
+import schemeproducerconsumer.visual.controllers.ErrorDialog;
 
 /**
- *
- * @author cesar
+ * @author César Arturo González Pérez
+ * @github arturogonzalezp
  */
+
 public class MainWindowController implements Initializable {
     @FXML
     private StackPane messagePane;
@@ -31,22 +22,7 @@ public class MainWindowController implements Initializable {
     
     @FXML
     private void runErrorDialog(ActionEvent event) {
-        JFXDialogLayout errorDialogLayout = new JFXDialogLayout();
-        errorDialogLayout.setHeading(new Label("Error"));
-        errorDialogLayout.setBody(new Text("Something went wrong"));
-        
-        JFXDialog errorDialog = new JFXDialog(messagePane,errorDialogLayout, JFXDialog.DialogTransition.CENTER);
-        
-        JFXButton okButton = new JFXButton("Ok");
-        okButton.setButtonType(JFXButton.ButtonType.FLAT);
-        okButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event1) {
-                errorDialog.close();
-            }
-        });
-        errorDialogLayout.setActions(okButton);
-        
+        ErrorDialog errorDialog = new ErrorDialog("Something went wrong",messagePane);
         errorDialog.show();
     }
     
---
+++ SchemeProducerConsumer/src/schemeproducerconsumer/visual/controllers/ErrorDialog.java
@@ -0,0 +1,49 @@
+package schemeproducerconsumer.visual.controllers;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialogLayout;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Cursor;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+
+/**
+ * @author César Arturo González Pérez
+ * @github arturogonzalezp
+ */
+
+public class ErrorDialog {
+    private JFXDialog dialog;
+    
+    public ErrorDialog(String errorMessage, StackPane stackPane){
+        JFXDialogLayout errorDialogLayout = new JFXDialogLayout();
+        errorDialogLayout.setHeading(new Label("Error"));
+        errorDialogLayout.setBody(new Text(errorMessage));
+        
+        dialog = new JFXDialog(stackPane,errorDialogLayout, JFXDialog.DialogTransition.CENTER);
+        
+        JFXButton okButton = new JFXButton("OK");
+        okButton.setButtonType(JFXButton.ButtonType.FLAT);
+        okButton.setCursor(Cursor.HAND);
+        okButton.setDefaultButton(true);
+        okButton.setPadding(new Insets(10));
+        okButton.setStyle("-fx-text-fill: #00acc1;");
+        okButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event1) {
+                close();
+            }
+        });
+        errorDialogLayout.setActions(okButton);
+    }
+    public void show(){
+        dialog.show();
+    }
+    public void close(){
+        dialog.close();
+    }
+}
